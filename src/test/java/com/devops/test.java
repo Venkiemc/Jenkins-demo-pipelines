11e40a5342b4bfe175fda4aeeadeebcdbb723fa7
Venkiemc <Venkatesh.m@errormakesclever.com>
2025-05-15 12:57:57 +0530

    Create test.java

---
+++ src/test/java/com/devops/test.java
@@ -0,0 +1,12 @@
+package com.devops;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testDummy() {
+        assertTrue(true); // placeholder test
+    }
+}
