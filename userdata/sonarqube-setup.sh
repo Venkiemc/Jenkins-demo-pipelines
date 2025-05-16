da4496e0310d115ddc9b7e00c3b8fcc9bf712bda
Venkiemc <Venkatesh.m@errormakesclever.com>
2025-05-16 12:42:09 +0530

    Update sonarqube-setup.sh

--- userdata/sonarqube-setup.sh
+++ userdata/sonarqube-setup.sh
@@ -1,50 +1,53 @@
 #!/bin/bash
+set -euxo pipefail
+
+# Backup and safely update sysctl settings
 cp /etc/sysctl.conf /root/sysctl.conf_backup
-cat <<EOT> /etc/sysctl.conf
-vm.max_map_count=262144
-fs.file-max=65536
-ulimit -n 65536
-ulimit -u 4096
-EOT
+echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+echo "fs.file-max=65536" >> /etc/sysctl.conf
+sysctl -p
+
+# Set user limits properly
 cp /etc/security/limits.conf /root/sec_limit.conf_backup
-cat <<EOT> /etc/security/limits.conf
-sonarqube   -   nofile   65536
-sonarqube   -   nproc    409
-EOT
+echo "sonarqube   -   nofile   65536" >> /etc/security/limits.conf
+echo "sonarqube   -   nproc    4096" >> /etc/security/limits.conf
 
-sudo apt-get update -y
-sudo apt-get install openjdk-21-jdk -y
-sudo update-alternatives --config java
+# Install Java
+apt-get update -y
+apt-get install openjdk-21-jdk -y
 
-java -version
+# Install PostgreSQL
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+apt-get update -y
+apt-get install postgresql postgresql-contrib -y
+systemctl enable postgresql
+systemctl start postgresql
 
-sudo apt update
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
-
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-sudo apt install postgresql postgresql-contrib -y
-#sudo -u postgres psql -c "SELECT version();"
-sudo systemctl enable postgresql.service
-sudo systemctl start  postgresql.service
-sudo echo "postgres:admin123" | chpasswd
+# Setup PostgreSQL user and DB for SonarQube
+echo "postgres:admin123" | chpasswd
 runuser -l postgres -c "createuser sonar"
-sudo -i -u postgres psql -c "ALTER USER sonar WITH ENCRYPTED PASSWORD 'admin123';"
-sudo -i -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonar;"
-sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube to sonar;"
-systemctl restart  postgresql
-#systemctl status -l   postgresql
-netstat -tulpena | grep postgres
-sudo mkdir -p /sonarqube/
+runuser -l postgres -c "psql -c \"ALTER USER sonar WITH ENCRYPTED PASSWORD 'admin123';\""
+runuser -l postgres -c "psql -c \"CREATE DATABASE sonarqube OWNER sonar;\""
+runuser -l postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonar;\""
+systemctl restart postgresql
+
+# Install SonarQube
+mkdir -p /sonarqube/
 cd /sonarqube/
-sudo curl -O https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-25.5.0.107428.zip
-sudo apt-get install zip -y
-sudo unzip -o sonarqube-25.5.0.107428.zip -d /opt/
-sudo mv /opt/sonarqube-25.5.0.107428/ /opt/sonarqube
-sudo groupadd sonar
-sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
-sudo chown sonar:sonar /opt/sonarqube/ -R
+curl -O https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-25.5.0.107428.zip
+apt-get install unzip -y
+unzip -o sonarqube-25.5.0.107428.zip -d /opt/
+mv /opt/sonarqube-25.5.0.107428/ /opt/sonarqube
+
+# Create user and assign ownership
+groupadd sonar
+useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
+chown -R sonar:sonar /opt/sonarqube/
+
+# Configure SonarQube
 cp /opt/sonarqube/conf/sonar.properties /root/sonar.properties_backup
-cat <<EOT> /opt/sonarqube/conf/sonar.properties
+cat <<EOT > /opt/sonarqube/conf/sonar.properties
 sonar.jdbc.username=sonar
 sonar.jdbc.password=admin123
 sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube
@@ -56,24 +59,21 @@ sonar.log.level=INFO
 sonar.path.logs=logs
 EOT
 
-cat <<EOT> /etc/systemd/system/sonarqube.service
+# Setup SonarQube systemd service
+cat <<EOT > /etc/systemd/system/sonarqube.service
 [Unit]
 Description=SonarQube service
 After=syslog.target network.target
 
 [Service]
 Type=forking
-
 ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
 ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
-
 User=sonar
 Group=sonar
 Restart=always
-
 LimitNOFILE=65536
 LimitNPROC=4096
-
 
 [Install]
 WantedBy=multi-user.target
@@ -81,14 +81,15 @@ EOT
 
 systemctl daemon-reload
 systemctl enable sonarqube.service
-#systemctl start sonarqube.service
-#systemctl status -l sonarqube.service
+
+# Install and configure NGINX as reverse proxy
 apt-get install nginx -y
-rm -rf /etc/nginx/sites-enabled/default
-rm -rf /etc/nginx/sites-available/default
-cat <<EOT> /etc/nginx/sites-available/sonarqube
-server{
-    listen      80;
+rm -f /etc/nginx/sites-enabled/default
+rm -f /etc/nginx/sites-available/default
+
+cat <<'EOT' > /etc/nginx/sites-available/sonarqube
+server {
+    listen 80;
     server_name sonarqube.groophy.in;
 
     access_log  /var/log/nginx/sonar.access.log;
@@ -101,19 +102,26 @@ server{
         proxy_pass  http://127.0.0.1:9000;
         proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
         proxy_redirect off;
-              
-        proxy_set_header    Host            \$host;
-        proxy_set_header    X-Real-IP       \$remote_addr;
-        proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
+
+        proxy_set_header    Host            $host;
+        proxy_set_header    X-Real-IP       $remote_addr;
+        proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header    X-Forwarded-Proto http;
     }
 }
 EOT
-ln -s /etc/nginx/sites-available/sonarqube /etc/nginx/sites-enabled/sonarqube
-systemctl enable nginx.service
-#systemctl restart nginx.service
-sudo ufw allow 80,9000,9001/tcp
 
-echo "System reboot in 30 sec"
-sleep 30
-reboot
+ln -s /etc/nginx/sites-available/sonarqube /etc/nginx/sites-enabled/sonarqube
+systemctl enable nginx
+
+# Allow necessary ports
+ufw allow 80,9000,9001/tcp || true
+
+# Optionally start services (uncomment if you want to start on boot)
+systemctl start sonarqube
+systemctl start nginx
+
+# Optional: reboot commented for safety
+# echo "System rebooting in 30 sec..."
+# sleep 30
+# reboot
